b57c369c1d8518ed163362ca4d79f8eb5ca9e54d
James Abels <jamesrabels@gmail.com>
2014-07-27 22:44:37 -0400

    Final commit for the night

---
+++ library/scss/config.rb
@@ -0,0 +1,15 @@
+require 'compass'
+require 'susy'
+require 'breakpoint'
+
+
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'library/css'
+sass_dir = 'library/scss/*.scss'
+images_dir = 'library/img'
+javascripts_dir = 'library/js'
+relative_assets = true
+line_comments = true
+output_style = :compressed
+sourcemap = true
