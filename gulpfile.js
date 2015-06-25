fbaeecc40029cddb46ab27205e5833e9c39ae585
James Abels <jamesrabels@gmail.com>
2015-06-25 18:06:31 -0400

    Merged changes from angulrar-boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Plugin Requires
 var livereloadForWas = require('gulp-livereload-for-was');
+var bootstrap = require('bootstrap-styl')
 var typographic = require('typographic');
 var stylish = require('jshint-stylish');
 var plumber = require('gulp-plumber');
@@ -8,17 +9,20 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rupture = require('rupture');
+var jeet = require('jeet');
 var gulp = require('gulp');
 var nib = require('nib');
 
 // PATHS
-var stylusDir = 'library/stylus/**/*.styl';
+var stylusDir = 'library/stylus/*.styl';
 var cssDir    = 'library/css';
 var jsDir     = 'library/js/*.js';
+var jsBS      = 'library/js/bootstrap/*.js';
 var jsVendor  = 'library/js/vendor/*.js';
-var jsMin     = 'library/js/min'
-var jsComp    = 'library/js/min/main.min.js'
-var allDir    =  './library/**/*.**'
+var jsMin     = 'library/js/min';
+var jsConcat  = 'library/js/!min/*.js';
+var jsComp    = 'library/js/min/main.min.js';
+var allDir    =  './library/**/*.**';
 var htmlDir   = '*.html';
 
 // TASKS
@@ -27,7 +31,7 @@ gulp.task('styles', function() {
   gulp.src(stylusDir)
     .pipe(plumber())
     .pipe(stylus({
-      use: [typographic(), nib(), rupture()],
+      use: [typographic(), nib(), rupture(), jeet(), bootstrap()],
       compress: true
     }))
     .pipe(gulp.dest(cssDir));
@@ -44,7 +48,7 @@ gulp.task('hint', function() {
 
 // Concat JS
 gulp.task('concat', function() {
-  return gulp.src(jsDir)
+  return gulp.src(['library/**/*.js', '!library/js/**/*.min.js'])
     .pipe(plumber())
     .pipe(concat('main.min.js'))
     .pipe(gulp.dest(jsMin));
@@ -62,9 +66,9 @@ gulp.task('minify', function() {
 // BUILD TASKS
 gulp.task('watch', function() {
   livereloadForWas.listen();
-  gulp.watch(stylusDir, ['styles'])
-  gulp.watch(jsDir, ['hint'])
-  gulp.watch(jsDir, ['concat'])
+  gulp.watch(stylusDir, ['styles']);
+  gulp.watch(jsDir, ['hint']);
+  gulp.watch(jsDir, ['concat']);
   gulp.watch(jsComp, ['minify'])
   gulp.watch(allDir).on('change', livereloadForWas.changed);
   gulp.watch(htmlDir).on('change', livereloadForWas.changed);
