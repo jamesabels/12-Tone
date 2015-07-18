dc2e712794881832c4fc920dfcdd04d05ffc7f54
jamesabels <jamesrabels@gmail.com>
2015-07-18 17:12:24 -0400

    Brought data.js over from old repo

---
+++ library/js/data.js
@@ -0,0 +1,6 @@
+var Cmaj = {
+  keyChar: "Completely pure. Its character is: innocence, simplicity, naïvety, children's talk.",
+  scale: ["C","D","E","F","G","A","B"],
+  chord: ["Cmaj","Dmin","Emin","Fmaj","Gmaj","Amin","Bdim"],
+  color: "#110a08"
+};
