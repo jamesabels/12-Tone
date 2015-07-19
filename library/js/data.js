0985c420c64dd73fab2c5e38bb03395a75fb538b
jamesabels <jamesrabels@gmail.com>
2015-07-19 15:19:05 -0400

    Sepertaing basic and sharp and flat keys until I can figure out how to handle them.

--- library/js/data.js
+++ library/js/data.js
@@ -4,6 +4,8 @@ var sharp = "&sharp;";
 
 var red = "#110a08";
 var yellow = "#FFFF00";
+var paleBlue = "#A2B5FF";
+
 
 // Major key roman numerals
 var majorNumerals = {
@@ -13,6 +15,9 @@ var majorNumerals = {
 var minorNumerals = {
     nums: ["i", "ii&deg;", "III", "iv", "v", "VI", "VII"]
 };
+
+
+// BASIC KEYS
 
 // C MAJOR
 var Cmaj = {
@@ -28,20 +33,6 @@ var Cmin = {
   color: red
 };
 
-// C# MAJOR
-var CSharpMaj = {
-  keyChar: "A leering key, degenerating into grief and rapture. It cannot laugh, but it can smile; it     cannot howl, but it can at least grimace its crying.",
-  scale: ["C&sharp;","D&sharp;","E&sharp;","F&sharp;","G&sharp;","A&sharp;","B&sharp;"],
-  color: red
-};
-
-// C# MINOR
-var CSharpMin = {
-  keyChar: "Penitential lamentation, intimate conversation with God, the friend and help-meet of life; sighs of disappointed friendship and love lie in its radius.",
-  scale: ["C&sharp;","D&sharp;","E","F&sharp;","G&sharp;","A","B"],
-  color: red
-};
-
 // D MAJOR
 var Dmaj = {
   keyChar: "The key of triumph, of Hallejuahs, of war-cries, of victory-rejoicing. Thus, the inviting       symphonies, the marches, holiday songs and heaven-rejoicing choruses are set in this key.",
@@ -54,4 +45,46 @@ var Dmin = {
   keyChar: "Melancholy womanliness, the spleen and humours brood.",
   scale: ["D","E","F","G","A","B&sharp;","C"],
   color: yellow
+};
+// E MAJOR
+var Emaj = {
+  keyChar: "Noisy shouts of joy, laughing pleasure and not yet complete, full delight lies in E Major.",
+  scale: ["E","F&sharp;","G&sharp;","A","B","C&sharp;","D&sharp;"],
+  color: paleBlue
+};
+
+// E MINOR
+var Emin = {
+  keyChar: "Naive, womanly innocent declaration of love, lament without grumbling; sighs accompanied by   few tears; this key speaks of the imminent hope of resolving in the pure happiness of C major. ",
+  scale: ["E","F&sharp;","G","A","B","C","D"],
+  color: paleBlue
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SHARP AND FLAT KEYS
+
+// C# MAJOR
+var CSharpMaj = {
+  keyChar: "A leering key, degenerating into grief and rapture. It cannot laugh, but it can smile; it     cannot howl, but it can at least grimace its crying.",
+  scale: ["C&sharp;","D&sharp;","E&sharp;","F&sharp;","G&sharp;","A&sharp;","B&sharp;"],
+  color: red
+};
+
+// C# MINOR
+var CSharpMin = {
+  keyChar: "Penitential lamentation, intimate conversation with God, the friend and help-meet of life; sighs of disappointed friendship and love lie in its radius.",
+  scale: ["C&sharp;","D&sharp;","E","F&sharp;","G&sharp;","A","B"],
+  color: red
 };
--- library/js/main.js
+++ library/js/main.js
@@ -18,28 +18,45 @@ var chordLabels = ".chord-labels";
 
 // CHANGE KEYS
 function changeKey() {
+    // BASIC KEYS
     // C MAJOR
     if ( $(keyMenu).val() === "c" && $(modeMenu).val() === "major") {
+        // Print Cmaj
         printKey(Cmaj);
     }
     // C MINOR
     if ( $(keyMenu).val() === "c" && $(modeMenu).val() === "minor") {
+        // Print Cmin
         printKey(Cmin);
-    }
-    // C# MAJOR
-    if ( $(keyMenu).val() === "c#" && $(modeMenu).val() === "major") {
-        printKey(CSharpMaj);
-    }
-    // C# MINOR
-    if ( $(keyMenu).val() === "c#" && $(modeMenu).val() === "minor") {
-        printKey(CSharpMin);
     }
     // D MAJOR
     if ( $(keyMenu).val() === "d" && $(modeMenu).val() === "major") {
+        // Print Dmaj
         printKey(Dmaj);
     }
     // D MINOR
     if ( $(keyMenu).val() === "d" && $(modeMenu).val() === "minor") {
+        // Print Dmin
         printKey(Dmin);
     }
+
+
+
+
+
+
+
+
+    // SHARP AND FLAT KEYS
+    // C# MAJOR or
+    if ($(keyMenu).val() === "c#" && $(modeMenu).val() === "major") {
+        // Print C#Maj
+        printKey(CSharpMaj);
+    }
+    // C# MINOR
+    if ( $(keyMenu).val() === "c#" && $(modeMenu).val() === "minor") {
+        // Print C#min
+        printKey(CSharpMin);
+    }
+
 }
