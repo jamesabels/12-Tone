95fcc1c30817995559543c4027639c5bd67a4e78
jamesabels <jamesrabels@gmail.com>
2015-07-29 13:20:52 -0400

    Converted structure to behave more like an app

--- src/js/swiper-settings.js
+++ src/js/swiper-settings.js
@@ -3,10 +3,14 @@
 // Init Swiper
 $(document).ready(function () {
   //initialize swiper when document ready  
-  var mySwiper = new Swiper ('.swiper-container', {
+  var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
+    paginationBulletRender: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+    paginationHide: false,
     direction: 'horizontal',
     loop: true,
     keyboardControl: true
-  })        
+  });
 });
