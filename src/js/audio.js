974bf9cea533edf72f191d4a3e75015e15940128
jamesabels <jamesrabels@gmail.com>
2015-08-06 09:50:01 -0400

    Testing Audio with PlayNote function

--- src/js/audio.js
+++ src/js/audio.js
@@ -3,7 +3,6 @@ var a = new Gibberish.Synth({ attack:44, decay:44100 }).connect();
  
 a.waveform = "Triangle"; 
 
-
-sectionTitles.click( function() {
-	a.note(880); 
-});  
+function playNote(freq) {
+	a.note(freq); 
+}
