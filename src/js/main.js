41c8b0fd15e7d558505b079fb60d137c9b34ae83
jamesabels <jamesrabels@gmail.com>
2015-08-24 10:43:20 -0400

    Added functionality to the back button.

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,20 @@
 $(document).ready(function () {
     printKey(key.major.C);
 });
+
+
+// Make back button toggle menu
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+    document.addEventListener("backbutton", function(e){
+       if( $('.key-section').hasClass("hide") ){
+         $(".key-section").addClass("shown");
+         $(".key-section").removeClass("hide");
+       }
+       else {
+         $(".key-section").addClass("hide");
+         $(".key-section").removeClass("shown");
+       }
+    }, false);
+}
