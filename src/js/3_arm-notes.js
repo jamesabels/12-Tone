d9a6e4ed684d657cb0cbb4d40c480e81c4834829
jamesabels <jamesrabels@gmail.com>
2015-08-25 09:27:15 -0400

    Added logic for second octave of notes.

--- src/js/3_arm-notes.js
+++ src/js/3_arm-notes.js
@@ -1,73 +1,169 @@
 function armNote(note) {
+	// C
 	if (note === "C") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.C);
 			src.play();
 		});
 	}
+	// C2
+	if (note === "C2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.C2);
+			src.play();
+		});
+	}
+	// C#
 	if (note === "C-Sharp") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.Csharp);
 			src.play();
 		});
 	}
+	// C2#
+	if (note === "C2-Sharp") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.C2sharp);
+			src.play();
+		});
+	}
+	// D
 	if (note === "D") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.D);
 			src.play();
 		});
 	}
+	// D2
+	if (note === "D2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.D2);
+			src.play();
+		});
+	}
+	// D#
 	if (note === "D-Sharp") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.Dsharp);
 			src.play();
 		});
 	}
+	// D2#
+	if (note === "D2-Sharp") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.D2sharp);
+			src.play();
+		});
+	}
+	// E
 	if (note === "E") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.E);
 			src.play();
 		});
 	}
+	// E2
+	if (note === "E2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.E2);
+			src.play();
+		});
+	}
+	// F
 	if (note === "F") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.F);
 			src.play();
 		});
 	}
+	// F2
+	if (note === "F2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.F2);
+			src.play();
+		});
+	}
+	// F#
 	if (note === "F-Sharp") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.Fsharp);
 			src.play();
 		});
 	}
+	// F#
+	if (note === "F2-Sharp") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.F2sharp);
+			src.play();
+		});
+	}
+	// G
 	if (note === "G") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.G);
 			src.play();
 		});
 	}
+	// G2
+	if (note === "G2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.G2);
+			src.play();
+		});
+	}
+	// G#
 	if (note === "G-Sharp") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.Gsharp);
 			src.play();
 		});
 	}
+	// G2#
+	if (note === "G2-Sharp") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.G2sharp);
+			src.play();
+		});
+	}
+	// A
 	if (note === "A") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.A);
 			src.play();
 		});
 	}
+	// A2
+	if (note === "A2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.A2);
+			src.play();
+		});
+	}
+	// A#
 	if (note === "A-Sharp") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.Asharp);
 			src.play();
 		});
 	}
+	// A2#
+	if (note === "A2-Sharp") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.A2sharp);
+			src.play();
+		});
+	}
+	// B
 	if (note === "B") {
 		$("." + note + "-note").click(function () {
 			var src = new Audio(instrument.piano.note.B);
+			src.play();
+		});
+	}
+	// B2
+	if (note === "B2") {
+		$("." + note + "-note").click(function () {
+			var src = new Audio(instrument.piano.note.B2);
 			src.play();
 		});
 	}
