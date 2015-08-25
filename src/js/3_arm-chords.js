f38a112ddf96123fdd5e3be629d4bd45f72ca09f
jamesabels <jamesrabels@gmail.com>
2015-08-25 09:20:33 -0400

    Added logic for second octave of chords

--- src/js/3_arm-chords.js
+++ src/js/3_arm-chords.js
@@ -6,10 +6,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// C2 major
+	if (note === "C2" && chord === "C2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.C2);
+			src.play();
+		});
+	}
 	// C# major
 	if (note === "C-Sharp" && chord === "C-Sharp-Major") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.major.Csharp);
+			src.play();
+		});
+	}
+	// C2# major
+	if (note === "C2-Sharp" && chord === "C2-Sharp-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.C2sharp);
 			src.play();
 		});
 	}
@@ -20,10 +34,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// C2 minor
+	if (note === "C2" && chord === "C2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.C2);
+			src.play();
+		});
+	}
 	// C# minor
 	if (note === "C-Sharp" && chord === "C-Sharp-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.Csharp);
+			src.play();
+		});
+	}
+	// C2# minor
+	if (note === "C2-Sharp" && chord === "C2-Sharp-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.C2sharp);
 			src.play();
 		});
 	}
@@ -34,10 +62,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// D2 major
+	if (note === "D2" && chord === "D2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.D2);
+			src.play();
+		});
+	}
 	// D# major
 	if (note === "D-Sharp" && chord === "D-Sharp-Major") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.major.Dsharp);
+			src.play();
+		});
+	}
+	// D2# major
+	if (note === "D2-Sharp" && chord === "D2-Sharp-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.D2sharp);
 			src.play();
 		});
 	}
@@ -48,10 +90,31 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// D minor
+	if (note === "D" && chord === "D-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.D);
+			src.play();
+		});
+	}
+	// D2 minor
+	if (note === "D2" && chord === "D2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.D2);
+			src.play();
+		});
+	}
 	// D# minor
 	if (note === "D-Sharp" && chord === "D-Sharp-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.Dsharp);
+			src.play();
+		});
+	}
+	// D2# minor
+	if (note === "D2-Sharp" && chord === "D2-Sharp-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.D2sharp);
 			src.play();
 		});
 	}
@@ -62,10 +125,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// E2 major
+	if (note === "E2" && chord === "E2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.E2);
+			src.play();
+		});
+	}
 	// E minor
 	if (note === "E" && chord === "E-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.E);
+			src.play();
+		});
+	}
+	// E2 minor
+	if (note === "E2" && chord === "E2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.E2);
 			src.play();
 		});
 	}
@@ -76,10 +153,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// F2 major
+	if (note === "F2" && chord === "F2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.F2);
+			src.play();
+		});
+	}
 	// F# major
 	if (note === "F-Sharp" && chord === "F-Sharp-Major") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.major.Fsharp);
+			src.play();
+		});
+	}
+	// F2# major
+	if (note === "F2-Sharp" && chord === "F2-Sharp-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.F2sharp);
 			src.play();
 		});
 	}
@@ -90,10 +181,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// F2 minor
+	if (note === "F2" && chord === "F2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.F2);
+			src.play();
+		});
+	}
 	// F# minor
 	if (note === "F-Sharp" && chord === "F-Sharp-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.Fsharp);
+			src.play();
+		});
+	}
+	// F2# minor
+	if (note === "F2-Sharp" && chord === "F2-Sharp-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.F2sharp);
 			src.play();
 		});
 	}
@@ -104,10 +209,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// G2 major
+	if (note === "G2" && chord === "G2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.G2);
+			src.play();
+		});
+	}
 	// G# major
 	if (note === "G-Sharp" && chord === "G-Sharp-Major") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.major.Gsharp);
+			src.play();
+		});
+	}
+	// G2# major
+	if (note === "G2-Sharp" && chord === "G2-Sharp-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.G2sharp);
 			src.play();
 		});
 	}
@@ -118,10 +237,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// G2 minor
+	if (note === "G2" && chord === "G2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.G2);
+			src.play();
+		});
+	}
 	// G# minor
 	if (note === "G-Sharp" && chord === "G-Sharp-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.Gsharp);
+			src.play();
+		});
+	}
+	// G2# minor
+	if (note === "G2-Sharp" && chord === "G2-Sharp-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.G2sharp);
 			src.play();
 		});
 	}
@@ -132,10 +265,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// A2 major
+	if (note === "A2" && chord === "A2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.A2);
+			src.play();
+		});
+	}
 	// A# major
 	if (note === "A-Sharp" && chord === "A-Sharp-Major") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.major.Asharp);
+			src.play();
+		});
+	}
+	// A2# major
+	if (note === "A2-Sharp" && chord === "A2-Sharp-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.A2sharp);
 			src.play();
 		});
 	}
@@ -146,10 +293,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// A2 minor
+	if (note === "A2" && chord === "A2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.A2);
+			src.play();
+		});
+	}
 	// A# minor
 	if (note === "A-Sharp" && chord === "A-Sharp-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.Asharp);
+			src.play();
+		});
+	}
+	// A2# minor
+	if (note === "A2-Sharp" && chord === "A2-Sharp-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.A2sharp);
 			src.play();
 		});
 	}
@@ -160,10 +321,24 @@ function armChords(note, chord) {
 			src.play();
 		});
 	}
+	// B2 major
+	if (note === "B2" && chord === "B2-Major") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.major.B2);
+			src.play();
+		});
+	}
 	// B minor
 	if (note === "B" && chord === "B-Minor") {
 		$("." + chord).click(function () {
 			var src = new Audio(instrument.piano.chord.minor.B);
+			src.play();
+		});
+	}
+	// B2 minor
+	if (note === "B2" && chord === "B2-Minor") {
+		$("." + chord).click(function () {
+			var src = new Audio(instrument.piano.chord.minor.B2);
 			src.play();
 		});
 	}
